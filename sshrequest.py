1dffd4e28310cb32f5b456cac83a36d865178f6c
HUSSEIN Adam <mh54500@gmail.com>
2023-03-17 21:25:23 +0100

    Add a functionality that generates a pie chart representing the number of times the command was typed

--- sshrequest.py
+++ sshrequest.py
@@ -1,17 +1,20 @@
 import datafetcher
 import paramiko
 from commandgrabber import CommandGrabber
+import pandas as pd
+from handlejson import HandleJson
+import matplotlib.pyplot as plt
 
 
 class SshRequest:
     """
-    A class for executing commands on remote Docker containers via SSH.
+        A class for executing commands on remote Docker containers via SSH.
 
-    Attributes:
-        None.
-
-    Methods:
-        main(): Executes commands on remote Docker containers using SSH connection.
+        Attributes:
+            None.
+    t
+        Methods:
+            main(): Executes commands on remote Docker containers using SSH connection.
     """
 
     def main(self):
@@ -30,14 +33,24 @@ class SshRequest:
 
         fetcher = datafetcher.DataFetcher()
         grabber = CommandGrabber()
+        handle = HandleJson()
         dict = fetcher.getJson("config.json")
+        columns = []
         cmd = ""
         cmd_park = input("Type exit to quit, what park do you want to select? ")
+        handle.create_json("commandes.json")
+
+        df = handle.get_json_data_to_dataframe("commandes.json")
+
+        # df = pd.read_json("commandes.json")
+
         while cmd != "exit":
             cmd = input("Type a command : ")
             if cmd == "change_park":
                 cmd_park = input("Type a command ")
                 continue
+            columns.append(cmd)
+            handle.add_to_dataframe(df, cmd)
             for container in dict:
                 if cmd_park in container["parks"]:
                     hostname = container["ip"]
@@ -59,5 +72,12 @@ class SshRequest:
                         output_sent = output.decode("utf-8")
                         grabber.send_commands("cmds_list.txt", cmd, output_sent)
 
-                        print(str(output, "utf8"))
+                        print(str(output, "utf8"))  # type: ignore
+        # plot = df.plot.pie()
+        plot_data = df[columns]
+        plot = plot_data.sum().plot(subplots=True, kind="pie")
+        plt.legend(columns)
+        # plot = df.plot(kind="pie", y=cmd)
+        plt.show()
+        handle.df_to_json(df, "commanded.json")
         return
